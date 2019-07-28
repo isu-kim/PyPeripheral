6a1b95b91b4240e59461acd8736e36d7683c1030
Isu Kim <49092508+gooday2die@users.noreply.github.com>
2019-07-29 01:06:56 +0900

    Update main.py

--- Codes/main.py
+++ Codes/main.py
@@ -8,4 +8,14 @@ from Wrappers.cue_sdk import *
 #Corsair.debugON()
 #Corsair.FirstInit()
 
-Corsair.DeviceInfo()
+#print(Corsair.DeviceInfo())
+#print(Corsair.DeviceList)
+#print(Corsair. DeviceID(Corsair.DeviceList))
+
+#Corsair.FirstInit()
+
+#print(Corsair.DeviceInfo())
+#Corsair.FirstInit()
+
+Corsair.debugOFF()
+Corsair.FirstInit()

f34f7288a0373eaa7e53c9877a37c913a00d5d17
Isu Kim <49092508+gooday2die@users.noreply.github.com>
2019-08-05 20:19:48 +0900

    updated

--- Codes/main.py
+++ Codes/main.py
@@ -1,72 +1,61 @@
 from Wrappers import Corsair
 from Wrappers import Razer
-from threading import Thread
-from Wrappers import requests
 import time
+import random
 from Wrappers.cue_sdk import *
 
 
-
-
-#Corsair.debugON()
-#Corsair.FirstInit()
-
-#print(Corsair.DeviceInfo())
-#print(Corsair.DeviceList)
-#print(Corsair. DeviceID(Corsair.DeviceList))
-
-#Corsair.FirstInit()
-
-#print(Corsair.DeviceInfo())
-#Corsair.FirstInit()
-
-Corsair.debugON()
-Corsair.FirstInit()
+Corsair.RequestControl()
+Corsair.debugOFF()
 
 RazerURI = Razer.geturi()
-Razer.debugON()
+Razer.debugOFF()
 
-def p1():
+
+
+
+def setEveryDeviceColor(r,g,b,duration):
+    Corsair.KeyboardSetdAll(r, g, b, duration)
+    Corsair.MM800SetAll(r, g, b, duration)
+    Razer.setEffect(Razer.createMouseEffect("CHROMA_STATIC", r, g, b, RazerURI), RazerURI)
+    Corsair.ledOn(CLK.GLAV_2, r, g, b, duration)
+    Corsair.ledOn(CLK.GLAV_1, r, g, b, duration)
+    Corsair.ledOn(CLK.GLAV_3, r, g, b, duration)
+    Corsair.ledOn(CLK.VOIDPRO_R, r, g, b, duration)
+    Corsair.ledOn(CLK.VOIDPRO_L, r, g, b, duration)
+    # duration == 0.001 is ideal
+
+def RainBowAll(step): # how fast the rainbow should be changed.
     while True:
-        for i in range(170, 188, 1):
+        for g in range(0,255,step):
+            #Corsair.KeyboardSetdAll(255, g, 0, 0.001)
+            setEveryDeviceColor(255,g,0,0.001)
 
-            Corsair.ledOn(i, 0, 255, 0, 0.01)
-            Corsair.ledOn(i + 1, 0, 255, 0, 0.01)
+        for r in range(255 , 0 , -step):
+            #Corsair.KeyboardSetdAll(r,255,0,0.001)
+            setEveryDeviceColor(r,255,0,0.001)
 
-        for i in range(170, 188, 1):
-            Corsair.ledOn(i, 0, 0, 0, 0.01)
-            Corsair.ledOn(i + 1, 0, 0, 0, 0.01)
+        for b in range(0,255,step):
+            #Corsair.KeyboardSetdAll(0,255,b,0.001)
+            setEveryDeviceColor(0,255,b,0.001)
 
-        for i in range(188, 170, -1):
-            Corsair.ledOn(i, 0, 255, 0, 0.01)
-            Corsair.ledOn(i + 1, 0, 255, 0, 0.01)
+        for g in range(255 , 0 , -step):
+            #Corsair.KeyboardSetdAll(0,g,255,0.001)
+            setEveryDeviceColor(0,g,255,0.001)
 
-        for i in range(188, 170, -1):
-            Corsair.ledOn(i, 0, 0, 0, 0.01)
-            Corsair.ledOn(i + 1, 0, 0, 0, 0.01)
+        for r in range(0,255,step):
+            #Corsair.KeyboardSetdAll(r,0,255,0.001)
+            setEveryDeviceColor(r,0,255,0.001)
+
+        for b in range(255 , 0 , -step):
+            #Corsair.KeyboardSetdAll(255,0,b,0.001)
+            setEveryDeviceColor(255,0,b,0.001)
 
 
-
-def p2():
+def randomColors():
     while True:
-        Effectid = Razer.createMouseEffect("CHROMA_STATIC" , 255, 0 , 0 , RazerURI)
-        Razer.setEffect(Effectid , RazerURI)
-        time.sleep(1)
-        Effectid = Razer.createMouseEffect("CHROMA_STATIC" , 0, 255 , 0 , RazerURI)
-        time.sleep(1)
-        Razer.setEffect(Effectid , RazerURI)
-        Effectid = Razer.createMouseEffect("CHROMA_STATIC" , 0, 0 , 255 , RazerURI)
-        time.sleep(1)
-        Razer.setEffect(Effectid , RazerURI)
+        for i in range(200):
+            Corsair.ledOn(i , random.randrange(1,256) , random.randrange(1,256) , random.randrange(1,256) , 0.0001)
+            Razer.setEffect(Razer.createMouseEffect("CHROMA_STATIC" , random.randrange(1,256) ,random.randrange(1,256) ,random.randrange(1,256) , RazerURI),RazerURI)
 
-def setall(r,g,b):
-    for i in range(200):
-        Corsair.ledOn(i , r,g,b,0.00001)
-    Effectid = Razer.createMouseEffect("CHROMA_STATIC", r, g, b, RazerURI)
-    Razer.setEffect(Effectid, RazerURI)
-
-
-while True:
-    setall(255 , 0 , 0 )
-    setall(0 , 255 , 0 )
-    setall(0 , 0 , 255 )
+RainBowAll(10)

f23a691934583bd30f70252d322259c6b898eacd
Isu Kim <49092508+gooday2die@users.noreply.github.com>
2019-12-14 15:58:54 +0900

    more PEP-8 Style!
    changed some parameters and classes.

--- Codes/screenreactive.py
+++ Codes/screenreactive.py
@@ -1,26 +1,26 @@
 from __future__ import print_function
-from Wrappers import Corsair
-from Wrappers import Razer
-from main import mainInit
 import main
 import numpy as np
 import scipy.cluster
 import mss
 from PIL import Image
-import time
+
 
 global debug
 debug = None
 
-def debugON():
+
+def debug_on():
     global debug
     debug = True
 
-def debugOFF():
+
+def debug_off():
     global debug
     debug = False
 
-def returnRGB():
+
+def return_rgb():
     with mss.mss() as sct:
         # Get rid of the first, as it represents the "All in One" monitor:
         for num, monitor in enumerate(sct.monitors[1:], 1):
@@ -31,77 +31,75 @@ def returnRGB():
             im = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
         im = im.resize((150, 150))      # optional, to reduce time
 
-        NUM_CLUSTERS = 1
-
+        num_clusters = 1
 
         ar = np.asarray(im)
         shape = ar.shape
         ar = ar.reshape(scipy.product(shape[:2]), shape[2]).astype(float)
 
-        codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
-        #Codes are stored as  a matrix
+        codes, dist = scipy.cluster.vq.kmeans(ar, num_clusters)
+        # Codes are stored as a matrix
         # 0 : R , 1 : G , 2 : B
 
         for i in range(len(codes)):
-            list = []
-            #print(int(codes[i][0]))
-            #print(int(codes[i][1]))
-            #print(int(codes[i][2]))
-            list.append(int(codes[i][0]))
-            list.append(int(codes[i][1]))
-            list.append(int(codes[i][2]))
+            lst = list()
+            lst.append(int(codes[i][0]))
+            lst.append(int(codes[i][1]))
+            lst.append(int(codes[i][2]))
 
-            '''
+        return lst
 
-            if debug:
-                print(' #%02x%02x%02x' % (int(codes[i][0]), int(codes[i][1]), int(codes[i][2])), end=' / ')
-            '''
-
-        return list
-'''
-while True:
-    returnRGB()
-    time.sleep(0.1)
-'''
 
 def ask():
     global debug
 
     print("[INFO] Turn Debug ON ?")
     answer = (input("True/False : "))
-    anslist = ["True", "False" , "true" , "false"]
-    posit = ["True", "true"]
-    negit = ["False" , "false"]
+    ans_list = ["True", "False", "true", "false"]
+    positive = ["True", "true"]
+    negative = ["False", "false"]
 
-    if answer not in anslist:
+    if answer not in ans_list:
         ask()
 
     else:
-        if answer in posit:
-            debugON()
+        if answer in positive:
+            debug_on()
 
-        if answer in negit:
-            debugOFF()
+        if answer in negative:
+            debug_off()
 
-    #print(debug)
 
-def screenreactive():
+def screen_reactive(obj_set_color):
     global debug
 
     while True:
-        newlist = returnRGB()
-        main.SetAllColor(newlist[0], newlist[1], newlist[2], 0)
+        new_list = return_rgb()
+        obj_set_color.all(new_list[0], new_list[1], new_list[2], 0)
+
         if debug:
-            print("[INFO] Dominant Color : ("+str(newlist[0])+"," +str(newlist[1])+","+str(newlist[2])+")")
+            print("[INFO] Dominant Color : ("+str(new_list[0])+"," + str(new_list[1])+","+str(new_list[2])+")")
 
 
 if __name__ == '__main__':
+    '''
     ask()
-    #print(debug)
+    print(debug)
+
     if debug:
         print("[INFO] Debug Turned ON")
     else:
         print("[INFO] Debug Turned OFF")
 
-    screenreactive()
+    '''
+    dev_list = main.DevList()
+    main_debug = main.Debug()
+    main_debug.all_on()
+    sdk_object = main.Sdks()
 
+    init = main.InitTools(main_debug, dev_list, sdk_object)
+    init.main_init()
+
+    set_color = main.SetColor(sdk_object)
+
+    screen_reactive(set_color)

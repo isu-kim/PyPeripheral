688b4758e3c1c9ede47be9b1a0b79b881fd6a031
Isu Kim <49092508+gooday2die@users.noreply.github.com>
2022-02-16 14:28:32 +0900

    Working on demo scripts
    Now demo scripts will inherit class "AbstractDemo" and will be overriding for polymorphism

--- Demos/ScreenReactive.py
+++ Demos/ScreenReactive.py
@@ -6,62 +6,92 @@
 """
 
 from __future__ import print_function
+
+import threading
 import numpy as np
 import scipy.cluster
 import mss
 from PIL import Image
 
 from PyPeripheral import All
+from abstractDemo import AbstractDemo
 
 
-def return_rgb():
-    """
-    A method that returns the most dominant color of current screen
-    This code is from https://stackoverflow.com/questions/3241929/python-find-dominant-most-common-color-in-an-image
-    Edited a bit of code in order to work in our demo script
-    :return: returns tuple type of rgb values of most dominant color.
-    """
-    with mss.mss() as sct:
-        # Get rid of the first, as it represents the "All in One" monitor:
-        for num, monitor in enumerate(sct.monitors[1:], 1):
-            # Get raw pixels from the screen
-            sct_img = sct.grab(monitor)
+class Demo(AbstractDemo):
+    def __init__(self):
+        """
+        An initializer method for class Demo in RainbowAll
+        """
+        self.thread = None
+        self.is_running = False
+        self.sdk_object = None
+        self.r = 0
+        self.g = 0
+        self.b = 0
 
-            # Create the Image
-            im = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
-        im = im.resize((150, 150))  # optional, to reduce time
+    def run(self, **kwargs):
+        """
+        An abstract method for running this demo.
+        This method will have main features of the demo script.
+        :return: returns None
+        """
+        self.thread = threading.Thread(target=self.__screen_reactive)
+        self.sdk_object = kwargs['sdk_object']
+        self.is_running = True
+        self.thread.start()
 
-        num_clusters = 1
+    def stop(self):
+        """
+        An abstract method for stopping this demo.
+        This method will terminate the demo script and this object.
+        :return: returns None
+        """
+        self.is_running = False
+        self.thread.join()
 
-        ar = np.asarray(im)
-        shape = ar.shape
-        ar = ar.reshape(np.product(shape[:2]), shape[2]).astype(float)
+    def __return_rgb(self):
+        """
+        A method that returns the most dominant color of current screen
+        This code is from https://stackoverflow.com/questions/3241929/python-find-dominant-most-common-color-in-an-image
+        Edited a bit of code in order to work in our demo script
+        :return: returns None
+        """
+        with mss.mss() as sct:
+            # Get rid of the first, as it represents the "All in One" monitor:
+            for num, monitor in enumerate(sct.monitors[1:], 1):
+                # Get raw pixels from the screen
+                sct_img = sct.grab(monitor)
 
-        codes, dist = scipy.cluster.vq.kmeans(ar, num_clusters)
-        # Codes are stored as a matrix
-        # 0 : R , 1 : G , 2 : B
+                # Create the Image
+                im = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
+            im = im.resize((150, 150))  # optional, to reduce time
 
-        for i in range(len(codes)):
-            lst = list()
-            lst.append(int(codes[i][0]))
-            lst.append(int(codes[i][1]))
-            lst.append(int(codes[i][2]))
+            num_clusters = 1
 
-        return lst
+            ar = np.asarray(im)
+            shape = ar.shape
+            ar = ar.reshape(np.product(shape[:2]), shape[2]).astype(float)
 
+            codes, dist = scipy.cluster.vq.kmeans(ar, num_clusters)
+            # Codes are stored as a matrix
+            # 0 : R , 1 : G , 2 : B
 
-def screen_reactive():
-    """
-    A method that does screen reactive lightning
-    :return: returns None
-    """
-    sdk_object = All.SDK()
-    sdk_object.connect()
-    while True:
-        new_list = return_rgb()
-        sdk_object.set_rgb({"ALL": (new_list[0], new_list[1], new_list[2])})
+            self.r = int(codes[0][0])
+            self.g = int(codes[0][1])
+            self.b = int(codes[0][2])
+
+    def __screen_reactive(self):
+        """
+        A method that does screen reactive lightning
+        :return: returns None
+        """
+        while self.is_running:
+            self.sdk_object.set_rgb({"ALL": (self.r, self.g, self.b)})
+        return
 
 
 if __name__ == '__main__':
-    screen_reactive()
-
+    sdk_object = All.SDK()
+    sdk_object.connect()
+    static_color = Demo()
+    static_color.run(sdk_object=sdk_object)
--- Demos/StaticColor.py
+++ Demos/StaticColor.py
@@ -5,22 +5,58 @@
 @file : StaticColor.py
 """
 
+import threading
+
 from PyPeripheral import All
+from abstractDemo import AbstractDemo
 
 
-def static_color(r, g, b):
-    """
-    A function that sets a single color over all devices
-    :param r: red value of the color
-    :param g: green value of the color
-    :param b: blue value of the color
-    :return: returns None
-    """
-    sdk_object = All.SDK()
-    sdk_object.connect()
-    while True:
-        sdk_object.set_rgb({"ALL": (r, g, b)})
+class Demo(AbstractDemo):
+    def __init__(self):
+        """
+        An initializer method for class Demo in RainbowAll
+        """
+        self.thread = None
+        self.is_running = False
+        self.sdk_object = None
+        self.r = 0
+        self.g = 0
+        self.b = 0
+
+    def run(self, **kwargs):
+        """
+        An abstract method for running this demo.
+        This method will have main features of the demo script.
+        :return: returns None
+        """
+        self.thread = threading.Thread(target=self.__static_color)
+        self.r = kwargs['r']
+        self.g = kwargs['g']
+        self.b = kwargs['b']
+        self.sdk_object = kwargs['sdk_object']
+        self.is_running = True
+        self.thread.start()
+
+    def stop(self):
+        """
+        An abstract method for stopping this demo.
+        This method will terminate the demo script and this object.
+        :return:
+        """
+        self.is_running = False
+        self.thread.join()
+
+    def __static_color(self):
+        """
+        A function that sets a single color over all devices
+        :return: returns None
+        """
+        while True:
+            self.sdk_object.set_rgb({"ALL": (self.r, self.g, self.b)})
 
 
 if __name__ == '__main__':
-    static_color(255, 0, 0)
+    sdk_object = All.SDK()
+    sdk_object.connect()
+    static_color = Demo()
+    static_color.run(sdk_object=sdk_object, r=255, g=255, b=255)
